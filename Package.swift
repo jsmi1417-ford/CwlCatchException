2885b109fbd2235578ca4f0e572ced68375dda46
jsmi1417-ford <39744622+jsmi1417-ford@users.noreply.github.com>
2021-09-21 01:45:59 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
 	name: "CwlCatchException",
+	platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v3), .tvOS(.v9)],
 	products: [
 		.library(name: "CwlCatchException", targets: ["CwlCatchException"]),
 	],
